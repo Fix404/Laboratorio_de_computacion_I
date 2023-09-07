0f8a15fc275af5ab15e8c4c26255f1a3f8d38040
Maximimiliano Niscola <niscolamaximiliano@gmail.com>
2023-09-06 22:35:42 -0300

    TP 3 casi terminado

--- Entregas_Ejercicios/Java/Main.java
+++ Entregas_Ejercicios/Java/Main.java
@@ -1,8 +1,10 @@
 public class Main {
   public static void main(String[] args) {
-    int num=3;
-    double num_d=num;
-    System.out.println(num);
-    System.out.println(num_d);
+    int C=-123;
+    System.out.println(C + (C<0 ? " es negativo " : " es positivo "));
+    System.out.println(C + (C%2==0 ? " es par " : " es impar "));
+    System.out.println(C + (C%5==0 ? " es múltiplo de 5 " : " no es múltiplo de 5 "));
+    System.out.println(C + (C%10==0 ? " es múltiplo de 10 " : " no es múltiplo 10 "));
+    System.out.println(C + (C<100 ? " es menor que 100 " : " no es menor que 100 "));
   }
 }
