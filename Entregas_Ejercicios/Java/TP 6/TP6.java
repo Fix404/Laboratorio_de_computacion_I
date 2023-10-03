cb88ecc3653388eb0ffde21a3f7e6ab2f417a6a2
Maximimiliano Niscola <niscolamaximiliano@gmail.com>
2023-10-02 22:10:20 -0300

    Inicio trabajo TP 6

--- Entregas_Ejercicios/Java/TP 6/TP6.java
+++ Entregas_Ejercicios/Java/TP 6/TP6.java
@@ -8,7 +8,7 @@ public class TP6 {
         float [] numbers= new float[10];
         for (int i = 0; i < numbers.length; i++){
             System.out.println("Ingrese el número entero de la posición "+i+": ");
-            numbers[i]=sc.nextInt();git add .
+            numbers[i]=sc.nextInt();
         }
 
         float negative_sum=0, negative_counter=0, positive_sum=0, positive_counter=0;
@@ -39,5 +39,25 @@ public class TP6 {
             positive_average=positive_sum/positive_counter;
             System.out.println("El promedio de los números positivos ingresados es de "+positive_average);
         }
+
+        // Consigna 2 //
+        float [] numbers_2= new float[10];
+        float even_position_sum=0, even_position_average=0;
+        int even_position_counter=0;
+
+        for (int i=0; i < numbers_2.length; i++){
+            System.out.println("Ingrese el número entero correspondiente a la posición "+i+": ");
+            numbers_2[i]=sc.nextInt();
+
+            if (i%2 == 0){
+                even_position_sum=even_position_sum+numbers_2[i];
+                ++even_position_counter;
+            }
+        }
+        even_position_average=even_position_sum/even_position_counter;
+        System.out.println("El promedio de los números ingresados que se encuentran en las posiciones pares es de "+even_position_average);
+
+        // Consigna 3 //
+        
+        }
     }
-}
