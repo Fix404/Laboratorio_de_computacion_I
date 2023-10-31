5cdf27895c9adcf87415bc79f46d62a1d63124af
Maximiliano Niscola <niscolamaximiliano@gmail.com>
2023-10-31 09:53:00 -0300

    TP 9 progreso

---
+++ Entregas_Ejercicios/Java/TP 9/TP_9.java
@@ -0,0 +1,69 @@
+import java.util.*;
+public class TP_9 {
+    public static void main(String[] args){
+        // TP 9 Colecciones //
+        Scanner sc=new Scanner(System.in);
+
+        // Consigna 1 //
+
+        /*ArrayList<Integer> numbers=new ArrayList<>();
+        int newNumber;
+
+        for (int i=0; i < 5; i++){
+            System.out.println("Ingrese un número entero a la lista: ");
+            newNumber=sc.nextInt();
+            numbers.add(newNumber);
+        }
+
+        System.out.println("Los números enteros en la lista son: ");
+        for (int j=0; j < 5; j++){
+            System.out.println(numbers.get(j));
+        } */
+
+        // Consigna 2 //
+
+        /*
+        ArrayList<String> words=new ArrayList<>();
+        String newWord;
+
+        for (int i=0; i < 3; i++){
+            System.out.println("Ingrese un nombre a la lista: ");
+            newWord=sc.next();
+            words.add(newWord);
+        }
+
+
+        System.out.println("Los nombres en la lista son: ");
+        for (int j=0; j < 3; j++){
+            System.out.println(words.get(j));
+        }
+        */
+        // Consigna 3 //
+
+        /* System.out.println("Los números enteros en la lista son: ");
+        for (int j=0; j < 5; j++){
+            System.out.println(numbers.get(j));
+        } */
+
+        // Consigna 4 //
+
+        /*
+        int amountOfWords=words.size();
+
+        System.out.println("La cantidad de palabras es de: "+amountOfWords);
+         */
+        // Consigna 5 //
+
+        /*
+        words.remove(2);
+        System.out.println("Los nombres en la lista son: ");
+        for (int j=0; j < words.size(); j++){
+            System.out.println(words.get(j));
+        }
+         */
+
+        // Consigna 6 //
+
+
+    }
+}
